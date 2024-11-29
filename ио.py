52f4e61cd26981c09a2a5e5b7fc006c8b8f26bc0
vetaxa90 <efimovich3@gmail.com>
2024-11-29 09:03:54 +0200

    Добавлено автоматическое копирование в буфер обмена

--- ио.py
+++ ио.py
@@ -3,6 +3,7 @@ from tkinter import filedialog as fd
 from  tkinter import messagebox as mb
 from tkinter import ttk
 import requests
+import  pyperclip
 
 def upload():
     try:
@@ -15,6 +16,8 @@ def upload():
                 link =response.json()["link"]
                 entry.delete(0,END)
                 entry.insert(0,link)
+                pyperclip.copy(link)
+                mb.showinfo("Ссылка скопирована"f"Ссылка {link} успешно скопирована в буфер обмена")
     except Exception as e:
         mb.showerror("Ошибка",f"Произошла ошибка :{e}")
 
